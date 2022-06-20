65a3691228599fc6386b59b47cd397353dc3dae1
Manthan-prasad <85788563+Manthan-prasad@users.noreply.github.com>
2022-06-20 20:54:16 +0530

    Add files via upload

--- Areaofcircles.py
+++ Areaofcircles.py
@@ -9,3 +9,7 @@ Created on Mon Jun 13 22:32:40 2022
 from math import pi
 r = float(input("Input the radius of the circle : "))
 print ("The area of the circle with radius ",r," is: ",pi*(r**2))
+
+filename = input("Input the Filename: ")
+f_extns = filename.split(".")
+print ("The extension of the file is : " + f_extns[1])
