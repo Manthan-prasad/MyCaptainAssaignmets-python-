01945502815fde91fd271af9c1f2ed2b09012a6b
Manthan-prasad <85788563+Manthan-prasad@users.noreply.github.com>
2022-07-03 17:01:09 +0530

    Add files via upload

---
+++ PositiveNumbersInRange.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul  3 16:57:23 2022
+
+@author: manthanprasad
+"""
+
+list1 = [25, -42, 31, 0, -85, 69]
+ 
+for num in list1:
+     
+    
+    if num >= 0:
+       print(num, end = " ")
